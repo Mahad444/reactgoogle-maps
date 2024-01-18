e78f695278752da0ae64f2cf0e92f81f772fe631
Ally Juma Said <mahadsaid95@icloud.com>
2024-01-18 23:54:53 +0000

    adding

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ export default function App() {
       >
         <Marker 
         coordinate={pin}
-        pinColor="red"
+        pinColor="blue"
         draggable={true}
         onDragStart={(e) => {
           console.log("Drag start", e.nativeEvent.coordinates)
